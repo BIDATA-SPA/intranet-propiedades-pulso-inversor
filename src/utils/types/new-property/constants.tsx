b8c1b0d67e59494ea5d747da2268d4cd4ead59da
jic-dv <jic-dev@outlook.com>
2026-03-23 14:45:49 -0300

    feat: add filter props on bedroom and bathroom types

--- src/utils/types/new-property/constants.tsx
+++ src/utils/types/new-property/constants.tsx
@@ -109,6 +109,7 @@ export const filterBathrooms: TSelect[] = [
   { value: '3', label: '3' },
   { value: '4', label: '4' },
   { value: '5', label: '5' },
+  { value: '+5', label: '+5' },
 ]
 
 export const filterBedrooms: TSelect[] = [
